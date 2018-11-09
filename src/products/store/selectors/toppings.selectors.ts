cd9dceb5b55e4b1f2f1692c84633d23d703e167a
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2018-11-09 14:10:54 +0100

    Continue with https://platform.ultimateangular.com/courses/ngrx-store-effects/lectures/3922946

---
+++ src/products/store/selectors/toppings.selectors.ts
@@ -0,0 +1,36 @@
+import { createSelector } from '@ngrx/store';
+
+import * as fromRoot from '../../../app/store';
+import * as fromFeature from '../reducers';
+import * as fromToppings from '../reducers/toppings.reducer';
+
+import { Topping } from '../../models/topping.model';
+
+export const getToppingsState = createSelector(
+  fromFeature.getProductsState,
+  (state: fromFeature.ProductsState) => state.toppings,
+);
+
+export const getToppingEntities = createSelector(
+  getToppingsState,
+  fromToppings.getToppingEntities,
+);
+
+export const getSelectedToppings = createSelector(
+  getToppingsState,
+  fromToppings.getSelectedToppings,
+);
+
+export const getAllToppings = createSelector(getToppingEntities, (entities) => {
+  return Object.keys(entities).map((id) => entities[parseInt(id, 10)]);
+});
+
+export const getToppingsLoaded = createSelector(
+  getToppingsState,
+  fromToppings.getToppingsLoaded,
+);
+
+export const getToppingsLoading = createSelector(
+  getToppingsState,
+  fromToppings.getToppingsLoading,
+);
