e5c96fce408e3bfe9a6965dcbabd2de9da0eabd9
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2019-02-27 18:42:23 +0100

    Preloading across multiple Routes. Continue with https://platform.ultimatecourses.com/courses/ngrx-store-effects/lectures/3922964

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -11,19 +11,15 @@ import { Pizza } from '../../models/pizza.model';
   template: `
     <div class="products">
       <div class="products__new">
-        <a
-          class="btn btn__ok" 
-          routerLink="./new">
+        <a class="btn btn__ok" routerLink="./new">
           New Pizza
         </a>
       </div>
       <div class="products__list">
-        <div *ngIf="!((pizzas$ | async)?.length)">
+        <div *ngIf="!(pizzas$ | async)?.length">
           No pizzas, add one to get started.
         </div>
-        <pizza-item
-          *ngFor="let pizza of (pizzas$ | async)"
-          [pizza]="pizza">
+        <pizza-item *ngFor="let pizza of (pizzas$ | async)" [pizza]="pizza">
         </pizza-item>
       </div>
     </div>
@@ -36,7 +32,6 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
     this.pizzas$ = this.store.select(fromStore.getAllPizzas);
-    this.store.dispatch(new fromStore.LoadPizzas());
     this.store.dispatch(new fromStore.LoadToppings());
   }
 }
