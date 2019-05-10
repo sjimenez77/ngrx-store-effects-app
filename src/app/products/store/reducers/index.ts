7a39b03b4c6590704a7ede9c976b90aa7e999c67
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2019-05-10 14:01:56 +0200

    Some imports reorganized.

--- src/app/products/store/reducers/index.ts
+++ src/app/products/store/reducers/index.ts
@@ -1,13 +1,7 @@
-import {
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-} from '@ngrx/store';
+import { EntityState } from '@ngrx/entity';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import * as fromPizzas from './pizzas.reducer';
 import * as fromToppings from './toppings.reducer';
-import { EntityState } from '@ngrx/entity';
-import { Pizza } from '../../models/pizza.model';
-import { Topping } from '../../models/topping.model';
 
 export interface ProductsState extends EntityState<any> {
   pizzas: fromPizzas.PizzaState;
