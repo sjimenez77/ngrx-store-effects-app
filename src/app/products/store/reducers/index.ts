0e24b3d41e7c1b0493c09d4c43de6a1a2356a685
Santos Jiménez <santos.jimenez-linares@capgemini.com>
2019-05-10 14:13:07 +0200

    ProductState extends EntityState with proper model types

--- src/app/products/store/reducers/index.ts
+++ src/app/products/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import { EntityState } from '@ngrx/entity';
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
+import { Pizza } from '../../models/pizza.model';
+import { Topping } from '../../models/topping.model';
 import * as fromPizzas from './pizzas.reducer';
 import * as fromToppings from './toppings.reducer';
 
-export interface ProductsState extends EntityState<any> {
+export interface ProductsState extends EntityState<Pizza | Topping> {
   pizzas: fromPizzas.PizzaState;
   toppings: fromToppings.ToppingsState;
 }
